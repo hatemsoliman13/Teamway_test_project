6b4109017180edb10fe6fef41ac4692debc8cd20
Hatem <hatem.soliman333@gmail.com>
2023-01-04 06:07:18 +0200

    refactor ShiftDuration enum and shifts apps

--- shifts/apps.py
+++ shifts/apps.py
@@ -6,19 +6,20 @@ from .enums import ShiftDuration
 class ShiftsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'shifts'
+    shift_duration = ShiftDuration
     validation_rules = {
         "create":
         {"worker_id": {"required": True, "type": int},
          "date": {"required": True, "type": date},
          "duration":
          {"required": True, "type": str,
-          "values": list(ShiftDuration)}},
+          "values": shift_duration.get_shift_duration_values()}},
         "update":
         {"shift_id": {"required": True, "type": int},
          "worker_id": {"required": True, "type": int},
          "date": {"required": True, "type": date},
          "duration":
          {"required": True, "type": str,
-          "values": list(ShiftDuration)}},
+          "values": shift_duration.get_shift_duration_values()}},
         "details": {"shift_id": {"required": True, "type": int}},
         "delete": {"shift_id": {"required": True, "type": int}}}
--- shifts/enums.py
+++ shifts/enums.py
@@ -5,3 +5,7 @@ class ShiftDuration(Enum):
     FIRST_SHIFT = "00-08"
     SECOND_SHIFT = "08-16"
     THIRD_SHIFT = "16-24"
+
+    @classmethod
+    def get_shift_duration_values(cls):
+        return list(map(lambda shift_duration: shift_duration.value, cls))
