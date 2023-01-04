ae40d00647e63e185e30dfb19e9df77707dc845a
Hatem <hatem.soliman333@gmail.com>
2023-01-04 05:42:51 +0200

    refactor requestvalidator

--- validators/requestvalidator.py
+++ validators/requestvalidator.py
@@ -13,7 +13,8 @@ class RequestValidator():
         for key, validation_rules in self.request_validation_rules.items():
             self.__validate_key(key, validation_rules)
         if not self.__is_valid():
-            raise RequestValidatorException(violations=self.__get_violations())
+            raise RequestValidatorException(
+                violations=self.__get_violations())
 
     def __get_violations(self) -> dict:
         return self.__violations
@@ -24,30 +25,44 @@ class RequestValidator():
     def __validate_key(self, key, validation_rules) -> None:
         for flag, value in validation_rules.items():
             if flag == "required" and value == True:
-                self.__does_exist(key)
+                self.__check_existence(key)
             if flag == "type":
-                self.__is_correct_type(key, value)
+                self.__check_correct_type(key, value)
             if flag == "values":
-                self.__has_correct_value(key, value)
+                self.__check_correct_value(key, value)
 
-    def __does_exist(self, key: str) -> None:
-        if key not in self.data:
+    def __check_existence(self, key: str) -> None:
+        if not self.__is_key_in_data(key):
             self.__report_violation(
                 "Missing ({key}) from request)".format(key=key))
-        elif not self.data[key]:
+        elif self.__is_empty(key):
             self.__report_violation(
                 "({key}) should not be empty)".format(key=key))
 
-    def __is_correct_type(self, key, type) -> None:
-        if not isinstance(self.data[key], type):
+    def __check_correct_type(self, key, type) -> None:
+        if self.__is_key_in_data(key) and not self.__has_correct_type(
+                key, type):
             self.__report_violation(
                 "({key}) has incorrect type, it should be ({correct_type})".
                 format(key=key, correct_type=type))
 
-    def __has_correct_value(self, key, values) -> None:
-        if self.data[key] not in values:
+    def __check_correct_value(self, key, values) -> None:
+        if self.__is_key_in_data(key) and not self.__has_correct_value(
+                key, values):
             self.__report_violation(
                 "Incorrect Value for ({key})".format(key=key))
 
     def __report_violation(self, violation: str) -> None:
         self.__violations.append(violation)
+
+    def __is_key_in_data(self, key: str) -> bool:
+        return key in self.data
+
+    def __is_empty(self, key: str) -> bool:
+        return not bool(self.data[key])
+
+    def __has_correct_type(self, key: str, type: type) -> bool:
+        return isinstance(self.data[key], type)
+
+    def __has_correct_value(self, key: str, values: list) -> bool:
+        return self.data[key] in values
